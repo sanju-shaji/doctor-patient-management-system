6d05617d5934cae212b4db86fe9d8b6ce2bf2213
sanju.shaji <sanjucrz007@gmail.com>
2025-03-24 12:57:28 +0530

    first commit

---
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/DoctorPatientInformationApplication.java
@@ -0,0 +1,13 @@
+package com.elixrlabs.doctorpatientmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorPatientInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorPatientInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elixrlabs/doctorpatientmanagementsystem/DoctorPatientInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixrlabs.doctorpatientmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorPatientInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
