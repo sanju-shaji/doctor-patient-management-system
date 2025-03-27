5c936fac22a517228ef22a882b52ef94ed27030e
tirupathi.mukala <tirupathimukala0808@gamil.com>
2025-03-27 13:06:45 +0530

    modified database name

--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/doctor/constants/DoctorConstants.java
+++
@@ -1,6 +0,0 @@
-package com.elixrlabs.doctorpatientmanagementsystem.doctor.constants;
-
-public class DoctorConstants {
-
-    public static final String DOCTOR_COLLECTION_NAME = "healthcare_system_patients";
-}
--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/model/doctor/DoctorEntity.java
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/model/doctor/DoctorEntity.java
@@ -1,6 +1,6 @@
-package com.elixrlabs.doctorpatientmanagementsystem.doctor.model;
+package com.elixrlabs.doctorpatientmanagementsystem.model.doctor;
 
-import com.elixrlabs.doctorpatientmanagementsystem.doctor.constants.DoctorConstants;
+import com.elixrlabs.doctorpatientmanagementsystem.constants.DataBaseConstants;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,10 +9,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
-@Document(collection = DoctorConstants.DOCTOR_COLLECTION_NAME)
+@Document(collection = DataBaseConstants.DOCTOR_COLLECTION_NAME)
 @Getter
 @Setter
 @AllArgsConstructor
+/** The entity class representing a doctor */
 public class DoctorEntity {
     @Id
     private UUID id;
