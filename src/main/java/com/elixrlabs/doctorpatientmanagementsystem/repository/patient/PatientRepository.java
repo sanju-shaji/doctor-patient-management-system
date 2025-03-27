d31df741ea6ac69bf84da6619c0a5a86b320e8fa
susmitha.mereddy <mereddysusmitha2002@gmail.com>
2025-03-27 22:21:17 +0530

    maintained class level documentation

--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/repository/patient/PatientRepository.java
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/repository/patient/PatientRepository.java
@@ -2,8 +2,13 @@ package com.elixrlabs.doctorpatientmanagementsystem.repository.patient;
 
 import com.elixrlabs.doctorpatientmanagementsystem.model.patient.PatientModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+/**
+ * Repository layer interface for patient entity
+ */
+@Repository
 public interface PatientRepository extends MongoRepository<PatientModel, UUID> {
 }
