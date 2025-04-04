75258c5cece6c11ba15d74a209f523028289e0eb
susmitha.mereddy <mereddysusmitha2002@gmail.com>
2025-04-04 10:40:37 +0530

    Arranged constants in order

--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/constants/ApplicationConstants.java
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/constants/ApplicationConstants.java
@@ -10,14 +10,16 @@ public class ApplicationConstants {
     public static final String EMPTY_LASTNAME = "Last name should not be an empty string or null";
     public static final String FIRSTNAME_PATTERN_ERROR = "First name should only contain alphabets";
     public static final String LASTNAME_PATTERN_ERROR = "Last name should only contain alphabets";
-    public static final String POST_DOCTOR_API = "/doctors";
-    public static final String REGEX_ALPHABET_PATTERN = "^[a-zA-Z\\s]+$";
-    public static final String REGEX_DEPARTMENTNAME_PATTERN = "^[a-zA-Z\\s\\-'.,]+$";
-    public static final String SERVER_ERROR = "Server error - ";
+    public static final String NO_PATIENTS_FOUND = "No patients found";
     public static final String PATIENT_FIRSTNAME_ERROR = "Patient first name cannot be empty or null.";
-    public static final String PATIENT_LASTNAME_ERROR = "Patient last name cannot be empty or null.";
     public static final String PATIENT_FIRSTNAME_PATTERN_ERROR = "Patient first name must contain only alphabet.";
+    public static final String PATIENT_LASTNAME_ERROR = "Patient last name cannot be empty or null.";
     public static final String PATIENT_LASTNAME_PATTERN_ERROR = "Patient last name must contain only alphabet.";
     public static final String PATIENTS_API = "/patients";
+    public static final String POST_DOCTOR_API = "/doctors";
+    public static final String QUERY_PARAMS_CANNOT_NULL = "Query params cannot be null";
+    public static final String REGEX_ALPHABET_PATTERN = "^[a-zA-Z\\s]+$";
+    public static final String REGEX_DEPARTMENTNAME_PATTERN = "^[a-zA-Z\\s\\-'.,]+$";
     public static final String REGEX_PATIENT_NAME_PATTERN = "^[a-zA-Z\\s]{1,50}$";
+    public static final String SERVER_ERROR = "Server error - ";
 }
--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/constants/PatientGetByNameConstants.java
+++
@@ -1,9 +0,0 @@
-package com.elixrlabs.doctorpatientmanagementsystem.constants;
-
-/**
- * class for storing all the string constants
- */
-public class PatientGetByNameConstants {
-    public static final String QUERY_PARAMS_CANNOT_NULL = "Query params cannot be null";
-    public static final String NO_PATIENTS_FOUND = "No patients found";
-}
