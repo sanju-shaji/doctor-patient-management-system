00ec0962c55daf7d4636ed4e16c997681da664e8
tirupathi.mukala <tirupathimukala0808@gamil.com>
2025-04-04 12:41:09 +0530

    edited constants file

--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/constants/ApplicationConstants.java
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/constants/ApplicationConstants.java
@@ -17,13 +17,11 @@ public class ApplicationConstants {
     public static final String LASTNAME_PATTERN_ERROR = "Last name should only contain alphabets";
     public static final String NO_PATIENTS_FOUND = "No patients found";
     public static final String PARAM_DOCTOR_NAME = "name";
-    public static final String QUERY_PARAMS_CANNOT_NULL = "Query params cannot be null";
-//    public static final String NO_PATIENTS_FOUND = "No patients found";
     public static final String PATIENT_FIRSTNAME_ERROR = "Patient first name cannot be empty or null.";
     public static final String PATIENT_FIRSTNAME_PATTERN_ERROR = "Patient first name must contain only alphabet.";
     public static final String PATIENT_LASTNAME_ERROR = "Patient last name cannot be empty or null.";
     public static final String PATIENT_LASTNAME_PATTERN_ERROR = "Patient last name must contain only alphabet.";
-//    public static final String QUERY_PARAMS_CANNOT_NULL = "Query params cannot be null";
+    public static final String QUERY_PARAMS_CANNOT_NULL = "Query params cannot be null";
     public static final String REGEX_ALPHABET_PATTERN = "^[a-zA-Z\\s]+$";
     public static final String REGEX_DEPARTMENTNAME_PATTERN = "^[a-zA-Z\\s\\-'.,]+$";
     public static final String REGEX_PATIENT_NAME_PATTERN = "^[a-zA-Z\\s]{1,50}$";
