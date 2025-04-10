baffac6943b58e62ae4fade17f51256b1e3179c7
tirupathi.mukala <tirupathimukala0808@gamil.com>
2025-04-11 00:49:51 +0530

    implemented list of error messagesfor for list of operation

--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/exceptionhandler/DataNotFoundException.java
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/exceptionhandler/DataNotFoundException.java
@@ -12,5 +12,4 @@ public class DataNotFoundException extends Exception {
         super(message);
         this.id = id;
     }
-
 }
--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/exceptionhandler/IdReplacementException.java
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/exceptionhandler/IdReplacementException.java
@@ -3,10 +3,10 @@ package com.elixrlabs.doctorpatientmanagementsystem.exceptionhandler;
 /**
  * Exception thrown when an attempt is made to modify the doctor's ID using a JSON Patch.
  */
-public class IdReplacementException extends RuntimeException{
-/**
- * Creates a new IdReplacementException with the specified error message.
- */
+public class IdReplacementException extends RuntimeException {
+    /**
+     * Creates a new IdReplacementException with the specified error message.
+     */
     public IdReplacementException(String errorMessage) {
         super(errorMessage);
     }
