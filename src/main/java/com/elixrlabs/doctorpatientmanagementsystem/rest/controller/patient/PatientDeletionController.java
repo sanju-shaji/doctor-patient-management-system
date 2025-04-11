11949772972d960fcf244be9ffabdbe90c1512c7
susmitha.mereddy <mereddysusmitha2002@gmail.com>
2025-04-11 12:09:20 +0530

    Resolved Conflicts

---
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/rest/controller/patient/PatientDeletionController.java
@@ -0,0 +1,29 @@
+package com.elixrlabs.doctorpatientmanagementsystem.rest.controller.patient;
+
+import com.elixrlabs.doctorpatientmanagementsystem.constants.ApiConstants;
+import com.elixrlabs.doctorpatientmanagementsystem.response.BaseResponse;
+import com.elixrlabs.doctorpatientmanagementsystem.service.patient.PatientDeletionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller responsible for handling requests related to deleting patient details who are not assigned to doctors.
+ */
+
+@RestController
+@RequestMapping(ApiConstants.PATIENT_API)
+public class PatientDeletionController {
+    private final PatientDeletionService patientDeletionResourceService;
+
+    public PatientDeletionController(PatientDeletionService patientDeletionResourceService) {
+        this.patientDeletionResourceService = patientDeletionResourceService;
+    }
+
+    @DeleteMapping(ApiConstants.PATIENT_ID)
+    public ResponseEntity<BaseResponse> ResponseDto(@PathVariable String patientId) throws Exception {
+        return patientDeletionResourceService.deletePatientById(patientId);
+    }
+}
---
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/service/patient/PatientDeletionService.java
@@ -0,0 +1,66 @@
+package com.elixrlabs.doctorpatientmanagementsystem.service.patient;
+
+import com.elixrlabs.doctorpatientmanagementsystem.enums.MessageKeyEnum;
+import com.elixrlabs.doctorpatientmanagementsystem.exceptionhandler.DataNotFoundException;
+import com.elixrlabs.doctorpatientmanagementsystem.exceptionhandler.EmptyUuidException;
+import com.elixrlabs.doctorpatientmanagementsystem.exceptionhandler.InvalidUuidExcetion;
+import com.elixrlabs.doctorpatientmanagementsystem.exceptionhandler.PatientAlreadyAssignedException;
+import com.elixrlabs.doctorpatientmanagementsystem.model.patient.PatientModel;
+import com.elixrlabs.doctorpatientmanagementsystem.repository.patient.DoctorPatientAssignmentRepository;
+import com.elixrlabs.doctorpatientmanagementsystem.repository.patient.PatientRepository;
+import com.elixrlabs.doctorpatientmanagementsystem.response.BaseResponse;
+import com.elixrlabs.doctorpatientmanagementsystem.util.MessageUtil;
+import com.elixrlabs.doctorpatientmanagementsystem.validation.patient.PatientValidation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Service class for deleting patient details who are not assigned to doctors.
+ */
+@Service
+public class PatientDeletionService {
+    private final PatientRepository patientRepository;
+    private final DoctorPatientAssignmentRepository doctorPatientAssignmentRepository;
+    private final PatientValidation patientValidation;
+    private final MessageUtil messageUtil;
+
+    public PatientDeletionService(PatientRepository patientRepository,
+                                  DoctorPatientAssignmentRepository doctorPatientAssignmentRepository,
+                                  PatientValidation patientValidation,
+                                  MessageUtil messageUtil) {
+        this.patientRepository = patientRepository;
+        this.doctorPatientAssignmentRepository = doctorPatientAssignmentRepository;
+        this.patientValidation = patientValidation;
+        this.messageUtil = messageUtil;
+    }
+
+    public ResponseEntity<BaseResponse> deletePatientById(String patientId) throws EmptyUuidException, DataNotFoundException, InvalidUuidExcetion, PatientAlreadyAssignedException {
+        patientValidation.validatePatientId(patientId);
+        Optional<PatientModel> patientModelOptional = patientRepository.findById(UUID.fromString(patientId));
+        if (!patientModelOptional.isPresent()) {
+            String message = messageUtil.getMessage(MessageKeyEnum.NO_PATIENT_FOUND.getKey());
+            throw new DataNotFoundException(message, UUID.fromString(patientId));
+        }
+        boolean isAssigned = !doctorPatientAssignmentRepository.findByPatientId(UUID.fromString(patientId)).isEmpty();
+        if (isAssigned) {
+            String message = messageUtil.getMessage(MessageKeyEnum.PATIENT_ASSIGNED_TO_DOCTOR.getKey());
+            throw new PatientAlreadyAssignedException(message);
+        }
+        patientRepository.deleteById(UUID.fromString(patientId));
+        String message = messageUtil.getMessage(MessageKeyEnum.PATIENT_DELETED_SUCCESSFULLY.getKey());
+        return buildSuccess(HttpStatus.OK, List.of(message));
+    }
+
+    private ResponseEntity<BaseResponse> buildSuccess(HttpStatus status, List<String> messages) {
+        BaseResponse baseResponse = BaseResponse.builder()
+                .success(true)
+                .messages(messages)
+                .build();
+        return new ResponseEntity<>(baseResponse, status);
+    }
+}
