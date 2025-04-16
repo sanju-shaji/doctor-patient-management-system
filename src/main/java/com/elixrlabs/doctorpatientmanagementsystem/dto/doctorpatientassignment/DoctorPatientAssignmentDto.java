e4e7c16973829de1c7e9c06fb9118fe069514d58
sanju.shaji <sanjucrz007@gmail.com>
2025-04-16 14:32:42 +0530

    added @SuperBuilder at DoctorPatientAssignmentDto

--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/dto/doctorpatientassignment/DoctorPatientAssignmentDto.java
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/dto/doctorpatientassignment/DoctorPatientAssignmentDto.java
@@ -1,17 +1,17 @@
 package com.elixrlabs.doctorpatientmanagementsystem.dto.doctorpatientassignment;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.util.UUID;
 
 /**
- *  DTO to transfer data of POST doctors api from controller to service layer
+ * DTO to transfer data of POST doctors api from controller to service layer
  */
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 public class DoctorPatientAssignmentDto {
     private UUID id;
     private UUID doctorId;
