f4a7fb40c0ebfa9b356b25f0bf896f55261cf383
soni <sonisebastian99@gmail.com>
2025-04-17 16:57:28 +0530

    Added constants file

--- src/main/java/com/elixrlabs/doctorpatientmanagementsystem/constants/ApiConstants.java
+++ src/main/java/com/elixrlabs/doctorpatientmanagementsystem/constants/ApiConstants.java
@@ -13,4 +13,5 @@ public class ApiConstants {
     public static final String PATIENT_API = "/patient";
     public static final String PATIENTS_API = "/patients";
     public static final String PATIENT_ID = "/{patientId}";
+    public static final String UN_ASSIGN_DOCTOR_FROM_PATIENT="UnAssignDoctorFromPatient";
 }
