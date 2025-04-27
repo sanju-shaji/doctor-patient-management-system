2ba05c45110a8b7f32997df2922f20f1ec73913f
soni <sonisebastian99@gmail.com>
2025-04-27 13:51:56 +0530

    Implemented Mockmvc in PatientDeletionControllerTest

--- src/test/java/com/elixrlabs/doctorpatientmanagementsystem/rest/controller/patient/PatientDeletionControllerTest.java
+++ src/test/java/com/elixrlabs/doctorpatientmanagementsystem/rest/controller/patient/PatientDeletionControllerTest.java
@@ -2,37 +2,44 @@ package com.elixrlabs.doctorpatientmanagementsystem.rest.controller.patient;
 
 import com.elixrlabs.doctorpatientmanagementsystem.constants.TestApplicationConstants;
 import com.elixrlabs.doctorpatientmanagementsystem.exceptionhandler.DataNotFoundException;
+import com.elixrlabs.doctorpatientmanagementsystem.exceptionhandler.GlobalExceptionHandler;
+import com.elixrlabs.doctorpatientmanagementsystem.exceptionhandler.PatientAlreadyAssignedException;
 import com.elixrlabs.doctorpatientmanagementsystem.response.BaseResponse;
-import com.elixrlabs.doctorpatientmanagementsystem.response.patient.PatientResponse;
 import com.elixrlabs.doctorpatientmanagementsystem.service.patient.PatientDeletionService;
-import com.elixrlabs.doctorpatientmanagementsystem.util.TestDataBuilder;
-import org.junit.jupiter.api.Assertions;
+import com.elixrlabs.doctorpatientmanagementsystem.util.MessageUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
-import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ExtendWith(MockitoExtension.class)
 class PatientDeletionControllerTest {
 
-    TestDataBuilder testDataBuilder;
+    private MockMvc mockMvc;
     @Mock
     private PatientDeletionService patientDeletionService;
 
-    @InjectMocks
-    private PatientDeletionController patientDeletionController;
+    @Mock
+    private MessageUtil messageUtil;
 
     @BeforeEach
     void setUp() {
-        testDataBuilder = new TestDataBuilder();
+        mockMvc = MockMvcBuilders.standaloneSetup(new PatientDeletionController(patientDeletionService)).setControllerAdvice(new GlobalExceptionHandler(messageUtil)).build();
     }
 
     /**
@@ -40,29 +47,45 @@ class PatientDeletionControllerTest {
      * HTTP Status Code-200
      */
     @Test
-    void testDeletePatientById_Success() throws Exception {
-        UUID patientId = UUID.randomUUID();
-        String patientIdString = patientId.toString();
-        PatientResponse patientResponse = testDataBuilder.patientResponseBuilder(patientId);
-        Mockito.when(patientDeletionService.deletePatientById(patientIdString)).thenReturn(ResponseEntity.ok(patientResponse));
-        ResponseEntity<BaseResponse> response = patientDeletionController.ResponseDto(patientIdString);
-        Assertions.assertEquals(200, response.getStatusCode().value());
-        Assertions.assertTrue(response.getBody().isSuccess());
-        Assertions.assertEquals(List.of(TestApplicationConstants.PATIENT_DELETED_SUCCESSFULLY), response.getBody().getMessages());
-        Mockito.verify(patientDeletionService, Mockito.times(1)).deletePatientById(patientIdString);
+    public void testDeletePatientById_Success() throws Exception {
+        BaseResponse baseResponse = new BaseResponse();
+        baseResponse.setSuccess(true);
+        baseResponse.setMessages(List.of(TestApplicationConstants.PATIENT_DELETED_SUCCESSFULLY));
+        ResponseEntity<BaseResponse> responseEntity = new ResponseEntity<>(baseResponse, HttpStatus.OK);
+        Mockito.when(patientDeletionService.deletePatientById(anyString())).thenReturn(responseEntity);
+        mockMvc.perform(delete("/patient/{patientId}", TestApplicationConstants.UUID).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.messages[0]").value(TestApplicationConstants.PATIENT_DELETED_SUCCESSFULLY));
     }
 
+    /**
+     * Test case for patient not found scenario
+     */
     @Test
-    void testDeletePatientById_DataNotFoundException() throws Exception {
-        UUID patientId = UUID.randomUUID();
-        String patientIdString = patientId.toString();
-        Mockito.when(patientDeletionService.deletePatientById(patientIdString)).thenThrow(new DataNotFoundException(TestApplicationConstants.NO_PATIENT_FOUND_WITH_ID));
-        try {
-            patientDeletionController.ResponseDto(patientIdString);
-            Assertions.fail(TestApplicationConstants.EXPECTED_DATA_NOT_FOUND_EXCEPTION);
-        } catch (DataNotFoundException exception) {
-            Assertions.assertEquals(TestApplicationConstants.NO_PATIENT_FOUND_WITH_ID, exception.getMessage());
-        }
-        Mockito.verify(patientDeletionService, Mockito.times(1)).deletePatientById(patientIdString);
+    public void testDeletePatientController_PatientNotFound() throws Exception {
+        Mockito.when(patientDeletionService.deletePatientById(anyString()))
+                .thenThrow(new DataNotFoundException(TestApplicationConstants.MOCK_EXCEPTION_MESSAGE));
+
+        mockMvc.perform(delete("/patient/{patientId}", TestApplicationConstants.UUID)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.errors").value(TestApplicationConstants.MOCK_EXCEPTION_MESSAGE));
+    }
+
+    /**
+     * Test case for patient already assigned scenario
+     */
+    @Test
+    public void testDeletePatientController_PatientAlreadyAssigned() throws Exception {
+        Mockito.when(patientDeletionService.deletePatientById(anyString()))
+                .thenThrow(new PatientAlreadyAssignedException(TestApplicationConstants.MOCK_EXCEPTION_MESSAGE));
+
+        mockMvc.perform(delete("/patient/{patientId}", TestApplicationConstants.UUID)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.errors").value(TestApplicationConstants.MOCK_EXCEPTION_MESSAGE));
     }
 }
--- src/test/java/com/elixrlabs/doctorpatientmanagementsystem/service/patient/PatientDeletionServiceTest.java
+++ src/test/java/com/elixrlabs/doctorpatientmanagementsystem/service/patient/PatientDeletionServiceTest.java
@@ -12,21 +12,12 @@ import com.elixrlabs.doctorpatientmanagementsystem.response.BaseResponse;
 import com.elixrlabs.doctorpatientmanagementsystem.util.MessageUtil;
 import com.elixrlabs.doctorpatientmanagementsystem.validation.patient.PatientValidation;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.verify;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -40,7 +31,7 @@ import java.util.UUID;
  * Test class for testing the PatientDeletion service Class
  */
 @ExtendWith(MockitoExtension.class)
-class PatientDeletionServiceTest {
+public class PatientDeletionServiceTest {
     @Mock
     private PatientRepository patientRepository;
 
@@ -65,14 +56,14 @@ class PatientDeletionServiceTest {
         String patientId = UUID.randomUUID().toString();
         PatientModel patient = new PatientModel();
         patient.setId(UUID.fromString(patientId));
-        when(patientRepository.findById(UUID.fromString(patientId))).thenReturn(Optional.of(patient));
-        when(doctorPatientAssignmentRepository.findByPatientId(UUID.fromString(patientId))).thenReturn(Collections.emptyList());
-        when(messageUtil.getMessage(MessageKeyEnum.PATIENT_DELETED_SUCCESSFULLY.getKey())).thenReturn(TestApplicationConstants.PATIENT_DELETED_SUCCESSFULLY);
+        Mockito.when(patientRepository.findById(UUID.fromString(patientId))).thenReturn(Optional.of(patient));
+        Mockito.when(doctorPatientAssignmentRepository.findByPatientId(UUID.fromString(patientId))).thenReturn(Collections.emptyList());
+        Mockito.when(messageUtil.getMessage(MessageKeyEnum.PATIENT_DELETED_SUCCESSFULLY.getKey())).thenReturn(TestApplicationConstants.PATIENT_DELETED_SUCCESSFULLY);
         ResponseEntity<BaseResponse> response = patientDeletionService.deletePatientById(patientId);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertTrue(response.getBody().getMessages().contains(TestApplicationConstants.PATIENT_DELETED_SUCCESSFULLY));
-        assertNull(response.getBody().getData());
-        verify(patientRepository, times(1)).deleteById(UUID.fromString(patientId));
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertTrue(response.getBody().getMessages().contains(TestApplicationConstants.PATIENT_DELETED_SUCCESSFULLY));
+        Assertions.assertNull(response.getBody().getData());
+        Mockito.verify(patientRepository, Mockito.times(1)).deleteById(UUID.fromString(patientId));
     }
 
     /**
@@ -83,14 +74,14 @@ class PatientDeletionServiceTest {
     void testDeletePatientById_PatientNotFound() throws Exception {
         String patientId = UUID.randomUUID().toString();
         String expectedMessage = new StringBuilder().append(TestApplicationConstants.NO_PATIENT_FOUND_WITH_ID).append(patientId).toString();
-        doNothing().when(patientValidation).validatePatientId(anyString());
-        when(patientRepository.findById(UUID.fromString(patientId))).thenReturn(Optional.empty());
-        when(messageUtil.getMessage(eq(MessageKeyEnum.NO_PATIENT_FOUND.getKey()), eq(patientId))).thenReturn(expectedMessage);
+        Mockito.doNothing().when(patientValidation).validatePatientId(Mockito.anyString());
+        Mockito.when(patientRepository.findById(UUID.fromString(patientId))).thenReturn(Optional.empty());
+        Mockito.when(messageUtil.getMessage(Mockito.eq(MessageKeyEnum.NO_PATIENT_FOUND.getKey()), Mockito.eq(patientId))).thenReturn(expectedMessage);
         try {
             patientDeletionService.deletePatientById(patientId);
-            fail(TestApplicationConstants.EXPECTED_DATA_NOT_FOUND_EXCEPTION);
+            Assertions.fail(TestApplicationConstants.EXPECTED_DATA_NOT_FOUND_EXCEPTION);
         } catch (DataNotFoundException exception) {
-            assertEquals(expectedMessage, exception.getMessage());
+            Assertions.assertEquals(expectedMessage, exception.getMessage());
         }
     }
 
@@ -103,14 +94,14 @@ class PatientDeletionServiceTest {
         String patientId = UUID.randomUUID().toString();
         PatientModel mockPatient = new PatientModel();
         String expectedMessage = new StringBuilder().append(TestApplicationConstants.NO_PATIENT_FOUND_WITH_ID).append(patientId).toString();
-        when(patientRepository.findById(UUID.fromString(patientId))).thenReturn(Optional.of(mockPatient));
-        when(doctorPatientAssignmentRepository.findByPatientId(UUID.fromString(patientId))).thenReturn(List.of(new DoctorPatientAssignmentModel()));
-        when(messageUtil.getMessage(MessageKeyEnum.PATIENT_ASSIGNED_TO_DOCTOR.getKey())).thenReturn(expectedMessage);
+        Mockito.when(patientRepository.findById(UUID.fromString(patientId))).thenReturn(Optional.of(mockPatient));
+        Mockito.when(doctorPatientAssignmentRepository.findByPatientId(UUID.fromString(patientId))).thenReturn(List.of(new DoctorPatientAssignmentModel()));
+        Mockito.when(messageUtil.getMessage(MessageKeyEnum.PATIENT_ASSIGNED_TO_DOCTOR.getKey())).thenReturn(expectedMessage);
         try {
             patientDeletionService.deletePatientById(patientId);
-            fail(TestApplicationConstants.EXPECTED_PATIENT_ALREADY_ASSIGNED_EXCEPTION);
+            Assertions.fail(TestApplicationConstants.EXPECTED_PATIENT_ALREADY_ASSIGNED_EXCEPTION);
         } catch (PatientAlreadyAssignedException exception) {
-            assertEquals(expectedMessage, exception.getMessage());
+            Assertions.assertEquals(expectedMessage, exception.getMessage());
         }
     }
 }
