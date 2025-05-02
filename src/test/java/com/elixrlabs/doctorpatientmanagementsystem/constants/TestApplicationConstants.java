6e2d659b51f2d8419b5d92910ac2834eae388a5d
SusmithaMereddy <mereddysusmitha2002@gmail.com>
2025-05-02 12:32:04 +0530

    Arranged constants in alphabetical order

--- src/test/java/com/elixrlabs/doctorpatientmanagementsystem/constants/TestApplicationConstants.java
+++ src/test/java/com/elixrlabs/doctorpatientmanagementsystem/constants/TestApplicationConstants.java
@@ -5,8 +5,8 @@ package com.elixrlabs.doctorpatientmanagementsystem.constants;
  */
 public class TestApplicationConstants {
     public static final String APPLICATION_JSON = "application/json";
-    public static final String DATA_NOT_FOUND_EXCEPTION_NOT_THROWN_MESSAGE = "Expected DataNotFoundException,but not thrown";
     public static final String BLANK_FIRST_NAME = " ";
+    public static final String DATA_NOT_FOUND_EXCEPTION_NOT_THROWN_MESSAGE = "Expected DataNotFoundException,but not thrown";
     public static final String DEPARTMENT_NAME = "departmentName";
     public static final String DOCTOR_DELETE_ENDPOINT = "/doctor/{doctorId}";
     public static final String DOCTOR_ENDPOINT = "/doctors";
@@ -15,9 +15,9 @@ public class TestApplicationConstants {
     public static final String EMPTY_QUERY_STRING = " ";
     public static final String EXCEPTION_NOT_THROWN_MESSAGE = "Expected InvalidAssignmentDataException was not thrown";
     public static final String EXPECTED_DATA_NOT_FOUND_EXCEPTION = "Expected an error when the patient with the given ID was not found";
-    public static final String EXPECTED_PATIENT_ALREADY_ASSIGNED_EXCEPTION = "Expected an error because the patient is already assigned to a doctor";
     public static final String EXPECTED_INVALID_USER_INPUT_EXCEPTION = "Expected InvalidUserInputException,but not thrown";
     public static final String EXPECTED_INVALID_UUID_EXCEPTION = "Expected InvalidUuidException,but not thrown";
+    public static final String EXPECTED_PATIENT_ALREADY_ASSIGNED_EXCEPTION = "Expected an error because the patient is already assigned to a doctor";
     public static final String FIRST_NAME = "firstName";
     public static final String GET_DOCTOR_BY_ID = "/doctors/{id}";
     public static final String INVALID_UUID = "12345";
@@ -30,7 +30,7 @@ public class TestApplicationConstants {
     public static final String NO_PATIENT_FOUND_WITH_ID = "No patient found with ID:";
     public static final String PATIENT_DELETED_SUCCESSFULLY = "Patient deleted successfully";
     public static final String PATIENT_ENDPOINT = "/patient/{patientId}";
-    public static final String QUERY_PARAM_NAME = "name";
     public static final String POST_PATIENTS_END_POINT = "/patients";
+    public static final String QUERY_PARAM_NAME = "name";
     public static final String UUID = "e562f728-1911-4ac5-91f0-de67772b2384";
 }
